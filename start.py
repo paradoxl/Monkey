105e39a3603e2fc9d12162b83f6ed55d1b2f981d
Michael Evans <35019721+paradoxl@users.noreply.github.com>
2022-05-15 20:54:42 -0500

    Add files via upload

---
+++ start.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+def typeShakespeare(scriptRead):
+    monkeyTyped = ''
+    while monkeyTyped == '' or scriptRead.count(monkeyTyped) >= 1:
+        monkeyTyped = monkeyTyped + (random.choice(string.ascii_lowercase + ' '))
+            
+    else:
+        return (monkeyTyped[:-1])
+
+script = open('test.txt','r') # opens the files
+scriptRead = script.read()
+script.close()
+
+keyPresses = 0
+monkeyTyped = ''
+while len(monkeyTyped) != len(scriptRead): ## infinite loop ... unless the monkey does it!
+
+    monkeyTyped = typeShakespeare(scriptRead)
+    keyPresses = keyPresses + len(monkeyTyped) + 1
+    
+    if len(monkeyTyped) >= 7:
+        monkeyTyped = "'"+monkeyTyped+"'"
+        print (monkeyTyped, keyPresses)
+    if keyPresses%10000==0:
+        print (keyPresses)
